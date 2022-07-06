7a38ad2cff658c4307a1c1694dcb827004fe6d93
kiwi <32146937+weinajin@users.noreply.github.com>
2022-07-06 11:47:48 -0700

    bib tag link open abstract, change preview+abbr div

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
     $('a.abstract').click(function() {
         $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
+        $(this).parent().parent().find(".bibtex.hidden").toggleClass("open", false);
     });
     $('a.bibtex').click(function() {
+        $(this).parent().parent().find(".abstract.hidden").toggleClass("open", false);
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     });
     $('.navbar-nav').find('a').removeClass('waves-effect waves-light');
+  // Open abstract when targeted (#bibkey in url)
+  $("div.row.publication-row:target").find(".abstract.hidden").addClass("open");
 });
